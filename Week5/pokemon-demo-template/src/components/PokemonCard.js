6a0f69cb9b94f96199f5a9b4336d8ec2fb15c60e
Jenna Lee <jenna.snow.lee@gmail.com>
2025-11-08 11:27:00 -0600

    pokemon

--- Week5/pokemon-demo-template/src/components/PokemonCard.js
+++ Week5/pokemon-demo-template/src/components/PokemonCard.js
@@ -9,14 +9,27 @@ import {
 import { Link } from "react-router-dom";
 
 function PokemonCard(props) {
-  const pokemonName = 'something';
+  const pokemonName = props.pokemon.name;
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.index}.png`;
 
   return (
     <Card sx={{ margin: 1 }}>
-
+      <CardActionArea component={Link} to={`/${pokemonName}`}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={imageUrl}
+          alt={pokemonName}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6">
+            {pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
+ 
 
 export default PokemonCard;
