2143d3c549f90744461b30a08a6464a9115ec29d
Jenna Lee <jenna.snow.lee@gmail.com>
2025-11-27 13:38:12 -0600

    fix: update Header component styles for consistency

--- Week5/homework/frontend/src/components/Header.js
+++ Week5/homework/frontend/src/components/Header.js
@@ -8,35 +8,35 @@ function Header() {
   return (
     <AppBar
       position="static"
-      backgroundColor='primary.main'
-      color="default"
+      backgroundColor='palette.primary.main'
+      color="primary"
       elevation={0}
       sx={{
         paddingY: 1,
-      margin: 0,
+        margin: 0,
         flexGrow: 1,
-          borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
+        borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
       }
-}
+      }
     >
-  <Toolbar>
-    <Typography variant="h4" sx={{ flexGrow: 1 }} fontWeight='bold'>
-      {currentUser ? `${currentUser.email}'s To-Do List` : "To-Do List"}
-    </Typography>
-    <Button
-      onClick={logout}
-      sx={{
-        backgroundColor: "#FFEBEB",
-        "&:hover": {
-          backgroundColor: "#FFCDCD",
-        },
-        color: "darkred",
-        textTransform: "none", // This will prevent text from being all uppercase
-      }}
-    >
-      Logout
-    </Button>
-  </Toolbar>
+      <Toolbar>
+        <Typography variant="h5" sx={{ flexGrow: 1 }} fontWeight='bold'>
+          {currentUser ? `${currentUser.email}'s To-Do List` : "To-Do List"}
+        </Typography>
+        <Button
+          onClick={logout}
+          sx={{
+            backgroundColor: "#ffffffff",
+            "&:hover": {
+              backgroundColor: "#FFCDCD",
+            },
+            color: "darkred",
+            textTransform: "none", // This will prevent text from being all uppercase
+          }}
+        >
+          Logout
+        </Button>
+      </Toolbar>
     </AppBar >
   );
 }
