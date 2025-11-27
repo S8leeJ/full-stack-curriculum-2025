b365ba45d9102674df20894243f41ff46415caec
Jenna Lee <jenna.snow.lee@gmail.com>
2025-11-27 12:26:32 -0600

    update creds

--- Week5/homework/backend/firebase.js
+++ Week5/homework/backend/firebase.js
@@ -1,7 +1,13 @@
 const admin = require("firebase-admin");
 const express = require("express");
-const creds = require("./cred.json"); // Path to your service account JSON
+// const creds = require("./cred.json"); // Path to your service account JSON
 // Initialize Firebase Admin SDK
+
+const creds = JSON.parse(
+  Buffer.from(process.env.FIREBASE_CREDS_BASE64, 'base64').toString()
+);
+
+
 admin.initializeApp({
   credential: admin.credential.cert(creds),
 });
